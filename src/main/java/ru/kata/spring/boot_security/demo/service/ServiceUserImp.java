feaea654be4b2b59e8afc783211a27463816f65e
Vladimir <yalt.vladimir@gmail.com>
2023-02-08 15:01:48 +0300

    Entity, DAO, Service packages are added

---
+++ src/main/java/ru/kata/spring/boot_security/demo/service/ServiceUserImp.java
@@ -0,0 +1,44 @@
+package ru.kata.spring.boot_security.demo.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import ru.kata.spring.boot_security.demo.dao.DaoUser;
+import ru.kata.spring.boot_security.demo.entity.User;
+
+import java.util.List;
+@Service
+public class ServiceUserImp implements ServiceUser{
+    @Autowired
+    private DaoUser daoUser;
+
+    @Override
+    @Transactional
+    public List<User> getAllUsers() {
+        return daoUser.getAllUsers();
+    }
+
+    @Override
+    @Transactional
+    public void addUser(User user) {
+        daoUser.addUser(user);
+    }
+
+    @Override
+    @Transactional
+    public User getUserById(Integer id) {
+        return daoUser.getUserById(id);
+    }
+
+    @Override
+    @Transactional
+    public void update(Integer id, User user) {
+        daoUser.update(id, user);
+    }
+
+    @Override
+    @Transactional
+    public void delete(Integer id) {
+        daoUser.delete(id);
+    }
+}
